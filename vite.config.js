0557ed216ee560b540b7d4ec838a1a9598c20161
mohamed dawood <mohameddawood17919@gmail.com>
2026-04-24 22:55:00 +0300

    Fix: Reverting to standard base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/mohamed-portfolio/',
   plugins: [react(), tailwindcss()],
 })
